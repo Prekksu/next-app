79256449c0e0ec9035c5fe64896c0d269d5a1ab4
Novrial Sandi <122582669+Prekksu@users.noreply.github.com>
2023-11-29 13:11:48 +0700

    init

---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./navbar";
+
+const ProfilePage = () => {
+	return (
+		<>
+			<div>ProfilePage</div>
+			<Navbar />
+		</>
+	);
+};
+
+export default ProfilePage;
